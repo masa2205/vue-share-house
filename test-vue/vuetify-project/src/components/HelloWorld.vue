6fa7211b0a928f0c54e6a09ef98a16670ecd1c63
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-03 03:32:41 +0900

    フッターの設定

--- test-vue/vuetify-project/src/components/HelloWorld.vue
+++ test-vue/vuetify-project/src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar color="error" dark>
+    <v-app-bar color="info" dark app>
       <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-app-bar>
+    <v-footer color="info" dark app>Vuetify</v-footer>
   </v-app>
 </template>
 
